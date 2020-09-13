ec438b6062ae0575ebbc5973de17294f8ddebebf
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-13 18:18:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,26 @@ class TicTacToe
   end 
   
   def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input_user)
+    input_user.to_i - 1 
+  end 
+  
+  def move(index, token = "X")
+    @board[index] = token 
+  end 
+  
+  def position_taken?(index)
+     @board[index] != " "
+  end 
+  
+  def valid_move?
     
   end 
 end 
