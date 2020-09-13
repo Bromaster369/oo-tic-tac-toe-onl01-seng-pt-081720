df29360ed9dfac241c365c4e0263aee09f29ede3
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-13 02:19:40 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
   
-  WIN_COMBINATIONS = [ [0], " ", " ", " ", " ", " ", " ", " "]
+  WIN_COMBINATIONS = [ [0], [1], [2], [3], [4], [5], [6], [7], [8] ]
   
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
